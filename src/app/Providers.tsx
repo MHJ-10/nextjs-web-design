3d7c0636c7e8c1b4d4379013b303f07efcc9d5c6
MHJ-10 <mhj10mhj10jafari@gmail.com>
2024-11-23 15:17:17 +0330

    build providers

---
+++ src/app/Providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import theme from "@/theme";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import React from "react";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
+};
+
+export default Providers;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,15 @@
-import theme from "@/theme";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
+import Providers from "./Providers";
 import Navbar from "./ui/Navbar";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Navbar />
-            {props.children}
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+        <Providers>
+          <Navbar />
+          {props.children}
+        </Providers>
       </body>
     </html>
   );
